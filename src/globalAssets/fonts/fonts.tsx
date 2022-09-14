f049c94d52124eca918d774ec08fd46ff01a288a
Karl Chvojka <karl.chvojka@protonmail.com>
2022-09-13 22:19:50 -0400

    STY: Add Fonts and config

--- src/globalAssets/fonts/fonts.tsx
+++ src/globalAssets/fonts/fonts.tsx
@@ -1,6 +1,11 @@
 // Library Imports
 import { createGlobalStyle } from 'styled-components'
 
+// Asset Imports
+import MonserratReg from 'globalAssets/fonts/Montserrat/static/Montserrat-Regular.ttf'
+import MonserratBold from 'globalAssets/fonts/Montserrat/static/Montserrat-Bold.ttf'
+import RobotoCond from 'globalAssets/fonts/Roboto_Condensed/RobotoCondensed-Regular.ttf'
+
 /**
  * Global Font styles
  * This is to abstract out the font imports.
@@ -11,6 +16,19 @@ import { createGlobalStyle } from 'styled-components'
   }
  */
 export default createGlobalStyle`
+  @font-face {
+    font-family: 'Montserrat';
+    src: url(${MonserratReg}) format('truetype');
+  }
 
+  @font-face {
+    font-family: 'MontserratBold';
+    src: url(${MonserratBold}) format('truetype');
+  }
+
+  @font-face {
+    font-family: 'Roboto-Condensed';
+    src: url(${RobotoCond}) format('truetype');
+  }
   
 `
