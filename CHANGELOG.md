184ed9d345f995428237988d6d1462f98679e409
Karl Chvojka <karl.chvojka@protonmail.com>
2022-09-14 00:27:47 -0400

    IMP: API Calls

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -56,3 +56,39 @@ Header
   - ./webpack.config.js
 - Header Styles
   - ./src/components/modules/Header/StyledHeader.tsx
+
+## [1.0.2] - 2022-09-13
+Api pulls
+
+### Added
+#### Libraries Added (./package.json)
+##### Dev Dependencies
+- @types/dotenv-webpack
+- dotenv-webpack
+
+#### Files added
+- .env
+  - Added to GitIgnore rules
+- .env.example file ./src/globalConfig/templates/
+- ./src/helpers
+  - api.js
+    - Helper functions to handle API calls.
+  - getPosition.js
+    - Gets your current location based off of browser
+  - isNight.js
+    - Checks to see if its night, returns booleen
+
+### Changed
+- .eslintrc
+  - Added Helper alias
+- index.tsx
+  - Activated React Strict
+- tsconfig.json
+  - Helpers alias
+- webpack.config.js
+  - Dotenv configuration
+- ./src/App.tsx
+  - Added state for position and weather response
+  - Set up use Effects
+  - render waits for state updates before rendering.
+  - else shows 'Loading'
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,56 @@
 // Framework Imports
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 
 // Library Imports
 import Header from 'modules/Header/Header'
+import { GetPosition } from 'helpers/getPosition'
+import { FetchWeather } from 'helpers/api'
 
 // CSS Imports
 import GlobalFonts from  'globalAssets/fonts/fonts'
 import GlobalStyle from 'src/globalConfig/GlobalStyles'
 import StyledApp from './StyledApp'
 
+// Type Declarations
+interface WeatherItem {
+  description?: string
+}
+
+interface weather {
+  current?: {
+    weather?: Array<WeatherItem>
+  }
+}
+
 const App = () => {
+  // State Declarations
+  const [pos, setPos] = useState([])
+  const [weather, setWeather] = useState<weather>({})
+
+  // UseEffect to handle getting position and setting pos state.
+  useEffect(() => {
+    GetPosition(setPos)
+  }, [])
+
+  // UseEffect to watch state and refresh on change.
+  useEffect(() => {
+    pos.length && FetchWeather(pos[0], pos[1]).then(response => {
+      setWeather(response.data)
+    })
+  }, [pos])
+
   return (
     <StyledApp>
       <GlobalFonts />
       <GlobalStyle />
       <Header />
       <h1>Welcome to the Weather App</h1>
+      {
+        Object.keys(weather).length > 0 ?
+          <p>{weather.current.weather[0].description}</p>
+          :
+          <p>Loading</p>
+      }
     </StyledApp>
   )
 }
